fd4469cb38497d6c876aa850da859b4d495ac410
Yacob Zitouni <yacob.zitouni@gmail.com>
2021-05-21 12:26:51 +0200

    cdup working directory mac

--- src/MininuxReplayManager/sources/main.cpp
+++ src/MininuxReplayManager/sources/main.cpp
@@ -1,9 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    //Set working directory because apple...
+    QDir bin(QCoreApplication::applicationDirPath());
+
+    #if defined(Q_WS_MAC)
+        bin.cdUp(); bin.cdUp(); bin.cdUp();
+    #endif
+
+     QDir::setCurrent(bin.absolutePath());
     MainWindow w;
     w.show();
     return app.exec();
